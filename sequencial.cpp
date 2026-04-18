609139330f75cdd26697a55d65f0a78328440a7c
exdjamm <25414650+exodojaffar@users.noreply.github.com>
2026-04-17 22:38:05 -0400

    feat: codigo sequencial do metodo de jacobian

--- sequencial.cpp
+++ sequencial.cpp
@@ -1,4 +1,6 @@
+#include <cmath>
 #include <ctype.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -20,12 +22,16 @@ int main(int argc, char const *argv[])
     }
     // Matrix use classe pois precisa armazenar coluna e linha para acesso.
     Matrix<float> A(argv[1]);
-    vector<float> B, X;
+    vector<float> B, X, X_k;
+
+    if (A.fail_load)
+        exit(EXIT_FAILURE);
 
     if (load_vector(argv[2], B))
         exit(EXIT_FAILURE);
 
-    X.assign(B.size(), 0.0);
+    X.assign(B.size(), 0.0f);
+    X_k.assign(B.size(), 0.0f);
 
     const unsigned int iterations = atoi(argv[3]);
     const float epsilon = atof(argv[4]);
@@ -33,6 +39,47 @@ int main(int argc, char const *argv[])
     printf("Ordem do Sistema: %d X %d\n", A.rows_number(), A.cols_number());
     printf("Maximo de Iteracoes: %d\n", iterations);
     printf("Epsilon: %f\n", epsilon);
+
+    unsigned int iter = 0;
+    float error, sum;
+
+    do
+    {
+        // Somatorio
+
+        for (size_t i = 0; i < A.rows_number(); i++)
+        {
+            sum = 0;
+            for (size_t j = 0; j < A.cols_number(); j++)
+            {
+                if (j != i)
+                {
+                    sum += A.get(i, j) * X[j];
+                }
+            }
+            X_k[i] = (B[i] - sum) / A.get(i, i);
+        }
+
+        error = 0;
+        for (size_t i = 0; i < X.size(); i++)
+        {
+            error += powf(X_k[i] - X[i], 2);
+            X[i] = X_k[i];
+        }
+
+        error = sqrtf(error);
+        iter++;
+    } while (iter < iterations && epsilon < error);
+
+    cout << "X = ";
+    for (size_t i = 0; i < X.size(); i++)
+    {
+        cout << X[i] << ", ";
+    }
+    cout << endl;
+
+    printf("Iteracoes: %d\n", iter);
+    printf("Delta X: %f\n", error);
 }
 
 int load_vector(string filename, vector<float> &vec)
